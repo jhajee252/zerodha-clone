2d203d0e5f08e56f819c11368285bdf69f923b87
Manish jha <mjha75197@gmail.com>
2026-04-10 14:57:33 +0530

    fix package.json eslint config

--- frontend/src/landing_page/products/Universe.js
+++ frontend/src/landing_page/products/Universe.js
@@ -1,51 +1,89 @@
-import React from 'react'
+import React from 'react';
 
 function Universe() {
     return ( 
         <div className='container mt-5'>
-           <div className='row text-center'>
+            <div className='row text-center'>
+                
                 <h1>The Zerodha Universe</h1>
                 <p>Extend your trading and investment experience even further with our partner platforms</p>
                 
                 <div className='col-4 p-3'>
-                    <img src="media/image/smallcaseLogo.png" alt="Smallcase Logo" style={{width:"45%"}}/>
-                    <p className='text-small text-muted mt-3'>Thematic investment platform</p>
+                    <img 
+                        src="media/image/smallcaseLogo.png" 
+                        alt="smallcase"
+                        style={{ width: "45%" }}
+                    />
+                    <p className='text-small text-muted mt-3'>
+                        Thematic investment platform
+                    </p>
                 </div>
 
                 <div className='col-4 p-3'>
-                    <img src="media/image/streakLogo.png" alt="Streak Logo" style={{width:"45%"}}/>
-                    <p className='text-small text-muted mt-3'>Algo & strategy platform</p>
+                    <img 
+                        src="media/image/streakLogo.png" 
+                        alt="streak"
+                        style={{ width: "45%" }}
+                    />
+                    <p className='text-small text-muted mt-3'>
+                        Algo & strategy platform
+                    </p>
                 </div>
 
                 <div className='col-4 p-3'>
-                    <img src="media/image/sensibullLogo.svg" alt="Sensibull Logo" style={{width:"45%"}}/>
-                    <p className='text-small text-muted mt-3'>Options trading platform</p>
+                    <img 
+                        src="media/image/sensibullLogo.svg" 
+                        alt="sensibull"
+                        style={{ width: "45%" }}
+                    />
+                    <p className='text-small text-muted mt-3'>
+                        Options trading platform
+                    </p>
                 </div>
                 
                 <div className='col-4 p-3 mt-5'>
-                    <img src="media/image/zerodhaFundhouse.png" alt="Zerodha Fundhouse" style={{width:"45%"}}/>
-                    <p className='text-small text-muted mt-3'>Asset management</p>
+                    <img 
+                        src="media/image/zerodhaFundhouse.png" 
+                        alt="fundhouse"
+                        style={{ width: "45%" }}
+                    />
+                    <p className='text-small text-muted mt-3'>
+                        Asset management
+                    </p>
                 </div>
 
                 <div className='col-4 p-3 mt-5'>
-                    <img src="media/image/goldenpiLogo.png" alt="Golden Pi" style={{width:"45%"}}/>
-                    <p className='text-small text-muted mt-3'>Bonds trading platform</p>
+                    <img 
+                        src="media/image/goldenpiLogo.png" 
+                        alt="goldenpi"
+                        style={{ width: "45%" }}
+                    />
+                    <p className='text-small text-muted mt-3'>
+                        Bonds trading platform
+                    </p>
                 </div>
 
                 <div className='col-4 p-3 mt-5'>
-                    <img src="media/image/dittoLogo.png" alt="Ditto Logo" style={{width:"25%"}}/>
-                    <p className='text-small text-muted mt-3'>Insurance</p>
+                    <img 
+                        src="media/image/dittoLogo.png" 
+                        alt="ditto"
+                        style={{ width: "25%" }}
+                    />
+                    <p className='text-small text-muted mt-3'>
+                        Insurance
+                    </p>
                 </div>
 
                 <button 
                     className='p-2 btn btn-primary fs-5 mb-5'  
-                    style={{width:"15%", margin: "0 auto"}}
+                    style={{ width: "15%", margin: "0 auto" }}
                 >
                     Signup Now
                 </button>
-           </div>
+
+            </div>
         </div> 
     );
-};
+}
 
 export default Universe;
--- frontend/src/landing_page/support/CreateTicket.js
+++ frontend/src/landing_page/support/CreateTicket.js
@@ -3,92 +3,93 @@ import { Link } from 'react-router-dom';
 
 function CreateTicket() {
     return ( 
-    <div className='container'>
-        <div className='row p-5 mt-5 mb-5'>
-            <h1>To create a ticket, select a relevant topic</h1>
+        <div className='container'>
+            <div className='row p-5 mt-5 mb-5'>
+                
+                <h1>To create a ticket, select a relevant topic</h1>
 
-            {/* Account Opening */}
-            <div className='col-4 p-5 mt-2 mb-2'>
-                <h4 className='fs-6'>
-                    <i className="fa fa-plus"></i> Account Opening
-                </h4>
+                {/* Account Opening */}
+                <div className='col-4 p-5 mt-2 mb-2'>
+                    <h4 className='fs-6'>
+                        <i className="fa fa-plus"></i> Account Opening
+                    </h4>
 
-                <Link to="/online-account" className="text-decoration-none d-block">Online Account Opening</Link>
-                <Link to="/offline-account" className="text-decoration-none d-block">Offline Account Opening</Link>
-                <Link to="/huf-account" className="text-decoration-none d-block">Company, Partnership and HUF Account Opening</Link>
-                <Link to="/nri-account" className="text-decoration-none d-block">NRI Account Opening</Link>
-                <Link to="/charges" className="text-decoration-none d-block">Charges at Zerodha</Link>
-                <Link to="/idfc-account" className="text-decoration-none d-block">Zerodha IDFC FIRST Bank 3-in-1 Account</Link>
-                <Link to="/getting-started" className="text-decoration-none d-block">Getting Started</Link>
+                    <Link to="/online-account" className="text-decoration-none d-block">Online Account Opening</Link>
+                    <Link to="/offline-account" className="text-decoration-none d-block">Offline Account Opening</Link>
+                    <Link to="/huf-account" className="text-decoration-none d-block">Company, Partnership and HUF Account Opening</Link>
+                    <Link to="/nri-account" className="text-decoration-none d-block">NRI Account Opening</Link>
+                    <Link to="/charges" className="text-decoration-none d-block">Charges at Zerodha</Link>
+                    <Link to="/idfc-account" className="text-decoration-none d-block">Zerodha IDFC FIRST Bank 3-in-1 Account</Link>
+                    <Link to="/getting-started" className="text-decoration-none d-block">Getting Started</Link>
+                </div>
+
+                {/* Account */}
+                <div className='col-4 p-5 mt-2 mb-2'>
+                    <h4 className='fs-6'>
+                        <i className="fa fa-user"></i> Your Zerodha Account
+                    </h4>
+
+                    <Link to="/login" className="text-decoration-none d-block">Login Credentials</Link>
+                    <Link to="/account-modification" className="text-decoration-none d-block">Account Modification and Segment Addition</Link>
+                    <Link to="/dp-details" className="text-decoration-none d-block">DP ID bank details</Link>
+                    <Link to="/profile" className="text-decoration-none d-block">Your Profile</Link>
+                    <Link to="/transfer-shares" className="text-decoration-none d-block">Transfer and conversion of shares</Link>
+                </div>
+
+                {/* Trading */}
+                <div className='col-4 p-5 mt-2 mb-2'>
+                    <h4 className='fs-6'>
+                        <i className="fa fa-bar-chart"></i> Trading
+                    </h4>
+
+                    <Link to="/margin" className="text-decoration-none d-block">Margin/leverage, Product and Order types</Link>
+                    <Link to="/kite" className="text-decoration-none d-block">Kite Web and Mobile</Link>
+                    <Link to="/trading-faq" className="text-decoration-none d-block">Trading FAQs</Link>
+                    <Link to="/corporate-actions" className="text-decoration-none d-block">Corporate Actions</Link>
+                    <Link to="/sentinel" className="text-decoration-none d-block">Sentinel</Link>
+                    <Link to="/kite-api" className="text-decoration-none d-block">Kite API</Link>
+                    <Link to="/pi" className="text-decoration-none d-block">Pi and other platforms</Link>
+                </div>
+
+                {/* Funds */}
+                <div className='col-4 p-5 mt-2 mb-2'>
+                    <h4 className='fs-6'>
+                        <i className="fa fa-credit-card"></i> Funds
+                    </h4>
+
+                    <Link to="/add-funds" className="text-decoration-none d-block">Adding Funds</Link>
+                    <Link to="/withdraw" className="text-decoration-none d-block">Funds Withdrawal</Link>
+                    <Link to="/emandates" className="text-decoration-none d-block">eMandates</Link>
+                    <Link to="/bank-account" className="text-decoration-none d-block">Adding Bank Account</Link>
+                </div>
+
+                {/* Console */}
+                <div className='col-4 p-5 mt-2 mb-2'>
+                    <h4 className='fs-6'>
+                        <i className="fa fa-circle-o-notch"></i> Console
+                    </h4>
+
+                    <Link to="/reports" className="text-decoration-none d-block">Reports</Link>
+                    <Link to="/leader" className="text-decoration-none d-block">Leader</Link>
+                    <Link to="/portfolio" className="text-decoration-none d-block">Portfolio</Link>
+                    <Link to="/ipo" className="text-decoration-none d-block">IPO</Link>
+                    <Link to="/referral" className="text-decoration-none d-block">Referral Program</Link>
+                </div>
+
+                {/* Coin */}
+                <div className='col-4 p-5 mt-2 mb-2'>
+                    <h4 className='fs-6'>
+                        <i className="fa fa-circle-o"></i> Coin
+                    </h4>
+
+                    <Link to="/mutual-funds" className="text-decoration-none d-block">Understanding Mutual Funds</Link>
+                    <Link to="/coin" className="text-decoration-none d-block">About Coin</Link>
+                    <Link to="/sip" className="text-decoration-none d-block">Starting an SIP</Link>
+                    <Link to="/government-securities" className="text-decoration-none d-block">Government Securities</Link>
+                </div>
+
             </div>
-
-            {/* Account */}
-            <div className='col-4 p-5 mt-2 mb-2'>
-                <h4 className='fs-6'>
-                    <i className="fa fa-user"></i> Your Zerodha Account
-                </h4>
-
-                <Link to="/login" className="text-decoration-none d-block">Login Credentials</Link>
-                <Link to="/account-modification" className="text-decoration-none d-block">Account Modification and Segment Addition</Link>
-                <Link to="/dp-details" className="text-decoration-none d-block">DP ID bank details</Link>
-                <Link to="/profile" className="text-decoration-none d-block">Your Profile</Link>
-                <Link to="/transfer-shares" className="text-decoration-none d-block">Transfer and conversion of shares</Link>
-            </div>
-
-            {/* Trading */}
-            <div className='col-4 p-5 mt-2 mb-2'>
-                <h4 className='fs-6'>
-                    <i className="fa fa-bar-chart"></i> Trading
-                </h4>
-
-                <Link to="/margin" className="text-decoration-none d-block">Margin/leverage, Product and Order types</Link>
-                <Link to="/kite" className="text-decoration-none d-block">Kite Web and Mobile</Link>
-                <Link to="/trading-faq" className="text-decoration-none d-block">Trading FAQs</Link>
-                <Link to="/corporate-actions" className="text-decoration-none d-block">Corporate Actions</Link>
-                <Link to="/sentinel" className="text-decoration-none d-block">Sentinel</Link>
-                <Link to="/kite-api" className="text-decoration-none d-block">Kite API</Link>
-                <Link to="/pi" className="text-decoration-none d-block">Pi and other platforms</Link>
-            </div>
-
-            {/* Funds */}
-            <div className='col-4 p-5 mt-2 mb-2'>
-                <h4 className='fs-6'>
-                    <i className="fa fa-credit-card"></i> Funds
-                </h4>
-
-                <Link to="/add-funds" className="text-decoration-none d-block">Adding Funds</Link>
-                <Link to="/withdraw" className="text-decoration-none d-block">Funds Withdrawal</Link>
-                <Link to="/emandates" className="text-decoration-none d-block">eMandates</Link>
-                <Link to="/bank-account" className="text-decoration-none d-block">Adding Bank Account</Link>
-            </div>
-
-            {/* Console */}
-            <div className='col-4 p-5 mt-2 mb-2'>
-                <h4 className='fs-6'>
-                    <i className="fa fa-circle-o-notch"></i> Console
-                </h4>
-
-                <Link to="/reports" className="text-decoration-none d-block">Reports</Link>
-                <Link to="/leader" className="text-decoration-none d-block">Leader</Link>
-                <Link to="/portfolio" className="text-decoration-none d-block">Portfolio</Link>
-                <Link to="/ipo" className="text-decoration-none d-block">IPO</Link>
-                <Link to="/referral" className="text-decoration-none d-block">Referral Program</Link>
-            </div>
-
-            {/* Coin */}
-            <div className='col-4 p-5 mt-2 mb-2'>
-                <h4 className='fs-6'>
-                    <i className="fa fa-circle-o"></i> Coin
-                </h4>
-
-                <Link to="/mutual-funds" className="text-decoration-none d-block">Understanding Mutual Funds</Link>
-                <Link to="/coin" className="text-decoration-none d-block">About Coin</Link>
-                <Link to="/sip" className="text-decoration-none d-block">Starting an SIP</Link>
-                <Link to="/government-securities" className="text-decoration-none d-block">Government Securities</Link>
-            </div>
-
         </div>
-    </div>
     );
 }
 

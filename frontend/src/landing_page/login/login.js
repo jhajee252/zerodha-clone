98b924f0a5adb4149dad4c836e6f789a98a4ecda
Manish jha <mjha75197@gmail.com>
2026-04-11 17:38:40 +0530

    fix: removed unused useNavigate

--- frontend/src/landing_page/login/login.js
+++ frontend/src/landing_page/login/login.js
@@ -1,7 +1,7 @@
 
 import React, { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 function Login() {
   const [data, setData] = useState({ email: "", password: "" });

afd0271e4c11bf30f243f5a89e232ce18757c48b
Manish jha <mjha75197@gmail.com>
2026-04-13 16:19:48 +0530

    fix: removed dashboard from home

--- dashboard/src/components/Home.js
+++ dashboard/src/components/Home.js
@@ -1,13 +1,28 @@
-import React from "react";
+// import React from "react";
 
-import Dashboard from "./Dashboard";
+// import Dashboard from "./Dashboard";
+// import TopBar from "./TopBar";
+
+// const Home = () => {
+//   return (
+//     <>
+//       <TopBar />
+//       <Dashboard />
+//     </>
+//   );
+// };
+
+// export default Home;
+
+
+import React from "react";
 import TopBar from "./TopBar";
 
 const Home = () => {
   return (
     <>
       <TopBar />
-      <Dashboard />
+      <h2>Welcome to Zerodha Clone 🚀</h2>
     </>
   );
 };

4d99538e838ef98ec6db83221883b297a7b8fb67
Manish jha <mjha75197@gmail.com>
2026-04-13 17:30:19 +0530

    fix: correct file casing login & signup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import HomePage from './landing_page/home/HomePage';
-import Signup from "./landing_page/signup/Signup";
+import Signup from "../landing_page/signup/Signup";
 import AboutPage from "./landing_page/about/AboutPage";
 import ProductsPage from "./landing_page/products/ProductsPage";
 import PricingPage from "./landing_page/pricing/PricingPage";
@@ -14,7 +14,7 @@ import SupportPage from "./landing_page/support/SupportPage";
 import NotFound from './landing_page/NotFound';
 import Navbar from "./Navbar";     
 import Footer from "./Footer";  
-import Login from "./landing_page/login/Login";
+import Login from "../landing_page/login/Login";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 

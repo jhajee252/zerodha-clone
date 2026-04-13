b8aeac4ddf87627106dceef36a4685a47da31e07
Manish jha <mjha75197@gmail.com>
2026-04-13 17:08:19 +0530

    fix: corrected signup casing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import HomePage from './landing_page/home/HomePage';
-import Signup from './landing_page/signup/Signup';
+import Signup from "./landing_page/signup/Signup"; 
 import AboutPage from "./landing_page/about/AboutPage";
 import ProductsPage from "./landing_page/products/ProductsPage";
 import PricingPage from "./landing_page/pricing/PricingPage";
